230e1485966fe38e2a1990dbe3807d57f7fc2152
avrilkamu <s22210220@student.unklab.ac.id>
2024-11-20 01:05:59 +0800

    edit firebase title footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
