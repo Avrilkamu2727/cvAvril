be3344ecfcd4b9d3a83131cd2875371cf7ce5f6a
avrilkamu <s22210220@student.unklab.ac.id>
2024-11-20 00:11:02 +0800

    firebase title header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,17 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect,useState } from "react";
 const Header = () => {
+    const [header, setHeader ] = useState ({})
+    useEffect (() => {
+        const db = getDatabase();
+        const headerRef = ref(db, "header");
+        onValue(headerRef, (snapshot) => {
+            const data = snapshot.val();
+            setHeader(data);
+        });
+    }, []);
     return (
+        
         <div className="container-fluid bg-primary d-flex align-items-center mb-5 py-5" id="home" style={{minHeight: '100vh'}}>
         <div className="container">
           <div className="row align-items-center">
@@ -7,9 +19,9 @@ const Header = () => {
               <img className="img-fluid w-100 rounded-circle shadow-sm" src="assets/img/profile.jpg" alt />
             </div>
             <div className="col-lg-7 text-center text-lg-left">
-              <h2 className="display text-uppercase text-primary mb-2" style={{WebkitTextStroke: 'px #000000'}}>I'm</h2>
-              <h1 className="display-2 text-uppercase text-primary mb-2" style={{WebkitTextStroke: '4px #000000'}}>Avril Agnes Kamu</h1>
-              <h3 className="text-white font-weight-normal mb-2" style={{WebkitTextStroke: '3px #00000'}}> Student in Klabat University</h3>
+              <h2 className="display text-uppercase text-primary mb-2" style={{WebkitTextStroke: '2px #000000'}}>{header.title2}</h2>
+              <h1 className="display-2 text-uppercase text-primary mb-2" style={{WebkitTextStroke: '4px #000000'}}>{header.title1}</h1>
+              <h3 className="text-white font-weight-normal mb-2" style={{WebkitTextStroke: '3px #00000'}}> {header.title3}</h3>
             </div>
           </div>
         </div>
